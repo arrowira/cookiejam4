ad87583d5e862f510cdaf4a53aff14930f595979
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-15 00:01:27 -0700

    anchor position moving

--- cookie-jam-4/player.gd
+++ cookie-jam-4/player.gd
@@ -3,7 +3,8 @@ extends Node2D
 var anchor = "red"
 var bSpinSpeed = 1
 var rSpinSpeed = 1
-
+@onready var red = $bSpin/red
+@onready var blue = $rSpin/blue
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,8 +14,10 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
 		if anchor == "red":
 			anchor = "blue"
+			$bSpin.global_position = blue.global_position
 		else:
 			anchor = "red"
+			$rSpin.global_position = red.global_position
 			
 func _physics_process(delta: float) -> void:
 	if anchor == "red":
