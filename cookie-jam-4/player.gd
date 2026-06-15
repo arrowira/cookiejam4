2b2508d7de333217fde06380c770e443ed8afbe6
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-15 00:10:06 -0700

    fixed telleporting on toggle

--- cookie-jam-4/player.gd
+++ cookie-jam-4/player.gd
@@ -5,6 +5,7 @@ var bSpinSpeed = 1
 var rSpinSpeed = 1
 @onready var red = $bSpin/red
 @onready var blue = $rSpin/blue
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,9 +15,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
 		if anchor == "red":
 			anchor = "blue"
+			$bSpin.rotation_degrees = $rSpin.rotation_degrees
 			$bSpin.global_position = blue.global_position
 		else:
 			anchor = "red"
+			$rSpin.rotation_degrees = $bSpin.rotation_degrees
 			$rSpin.global_position = red.global_position
 			
 func _physics_process(delta: float) -> void:
