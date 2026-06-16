b75918b86cb98255c85e4c5f0fda776d61147177
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-16 16:34:05 -0700

    take damage UI

--- cookie-jam-4/scripts/HeartManager.gd
+++ cookie-jam-4/scripts/HeartManager.gd
@@ -4,6 +4,7 @@ extends Panel
 var hearts = []
 var pRHeart = preload("res://scenes/red_heart.tscn")
 var pBHeart = preload("res://scenes/blue_heart.tscn")
+var health = 0
 
 func addHeart(color):
 	var heart
@@ -13,8 +14,18 @@ func addHeart(color):
 		heart = pBHeart.instantiate()
 	heart.position = Vector2(len(hearts)*50 + 40, position.y+10)
 	hearts.append(heart)
+	health += 1
 	add_child(heart)
+	
+func takeDamage():
+	health -= 1
+	flipHeart(health,1)
+
+func flipHeart(index, bit):
+	hearts[index].frame = bit
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space"):
 		addHeart("red")
+	if Input.is_action_just_pressed("k"):
+		takeDamage()
