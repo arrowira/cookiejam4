10ef772ac9358f88b2f6c804e37c8830761da9b5
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-15 20:52:02 -0700

    fate

--- cookie-jam-4/enemy_manager.gd
+++ cookie-jam-4/enemy_manager.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var pGrunt = preload("res://scenes/grunt.tscn")
+
+var population = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,7 +14,11 @@ func _process(delta: float) -> void:
 
 
 func _on_spawn_cd_timeout() -> void:
-	var grunt = pGrunt.instantiate()
-	grunt.global_position.x = randf_range(-1000,1000)
-	grunt.global_position.y = randf_range(-1000,1000)
-	add_child(grunt)
+	var fate = randi_range(1,2)
+	
+	if fate == 1:
+		population+=1
+		var grunt = pGrunt.instantiate()
+		grunt.global_position.x = randf_range(-1000,1000)
+		grunt.global_position.y = randf_range(-1000,1000)
+		add_child(grunt)
