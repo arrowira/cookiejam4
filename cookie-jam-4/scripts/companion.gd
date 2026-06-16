b3c3447905fbf48fba016b55c3b69a41fd3a8672
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-15 23:00:16 -0700

    basic animations imported

--- cookie-jam-4/scripts/companion.gd
+++ cookie-jam-4/scripts/companion.gd
@@ -11,6 +11,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func anchor():
+	weapon = false
+	$AnimationPlayer.play("anchored")
+func deanchor():
+	weapon = true
+	$AnimationPlayer.play("unanchored")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
