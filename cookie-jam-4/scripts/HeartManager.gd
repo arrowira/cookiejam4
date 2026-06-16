6215b025d85df08691ceca845d37c2dc27f1b4df
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-16 16:38:23 -0700

    heart manager working

--- cookie-jam-4/scripts/HeartManager.gd
+++ cookie-jam-4/scripts/HeartManager.gd
@@ -14,12 +14,15 @@ func addHeart(color):
 		heart = pBHeart.instantiate()
 	heart.position = Vector2(len(hearts)*50 + 40, position.y+10)
 	hearts.append(heart)
-	health += 1
+	heal()
 	add_child(heart)
 	
 func takeDamage():
 	health -= 1
 	flipHeart(health,1)
+func heal():
+	health += 1
+	flipHeart(health-1,0)
 
 func flipHeart(index, bit):
 	hearts[index].frame = bit
@@ -29,3 +32,5 @@ func _process(delta: float) -> void:
 		addHeart("red")
 	if Input.is_action_just_pressed("k"):
 		takeDamage()
+	if Input.is_action_just_pressed("i"):
+		heal()
