6023a2af20f3efd20d3f3f0f6608b67ff07198a6
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-18 15:19:35 -0700

    unfinished ghost alpha fix

--- cookie-jam-4/scripts/ghost.gd
+++ cookie-jam-4/scripts/ghost.gd
@@ -3,9 +3,17 @@ extends Node2D
 var t = 200.0
 var maxT = 200.0
 
+var ghostShader = preload("res://ghost.gdshader")
+
 func _ready() -> void:
-	maxT = t
+	#maxT = t
+	#var newMaterial = ShaderMaterial.new()
+	#newMaterial.shader = ghostShader
+	#var texture = load("res://sprites/Zombie_Monkey_Sprite_Sheet.webp")
+	#newMaterial.set_shader_parameter("Tex", texture)
+	#$Icon.material = newMaterial
 	
+	pass
 
 func _physics_process(delta: float) -> void:
 	$Icon.get_material().set_shader_parameter("alpha", t/maxT)
