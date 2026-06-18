edb9e618e842ae78cf6e9066a15ed451b903fb92
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-18 03:59:57 -0700

    ghost fade outs

--- cookie-jam-4/scripts/ghost.gd
+++ cookie-jam-4/scripts/ghost.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
-var t = 200
+var t = 200.0
+var maxT = 200.0
+
+func _ready() -> void:
+	maxT = t
+	
+
 func _physics_process(delta: float) -> void:
+	$Icon.get_material().set_shader_parameter("alpha", t/maxT)
+	
 	position.y-=5
 	t-=1
 	if t == 0:
