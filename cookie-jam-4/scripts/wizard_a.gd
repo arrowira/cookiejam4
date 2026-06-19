0324814780e7598b0fe0c73c404dfd0684a54822
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-19 16:55:27 -0700

    created wizardA scene

---
+++ cookie-jam-4/scripts/wizard_a.gd
@@ -0,0 +1,74 @@
+extends RigidBody2D
+
+var player = Vector2.RIGHT
+var health = 100
+var towardsPlayer = Vector2.RIGHT
+var inKB = false
+var frozen = false
+var dead = false
+
+var pGhost = preload("res://scenes/grunt_ghost.tscn")
+var speed = 1.5
+
+func _ready() -> void:
+	$healthBar.value = 100
+	
+	$Icon.modulate.b -= randf()/3.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !frozen:
+		$AnimationPlayer.play("walk")
+		if player.x<global_position.x:
+			$Icon.flip_h=false
+			$Icon/shadow.position = Vector2(0.8,7.8)
+		else:
+			$Icon.flip_h=true
+			$Icon/shadow.position = Vector2(-0.8,7.8)
+		
+		player = get_parent().get_parent().get_node("player").anchorPos
+	
+
+func _physics_process(delta: float) -> void:
+	rotation = 0
+	if !inKB and !frozen:
+		towardsPlayer = (player-global_position).normalized()
+		position+=towardsPlayer*speed*Engine.time_scale
+
+func death():
+	dead = true
+	frozen = true
+	var ghost = pGhost.instantiate()
+	ghost.global_position=global_position
+	get_parent().add_child(ghost)
+	
+	$deathTimer.start()
+	$AnimationPlayer.play("die")
+	$healthBar.visible = false
+	
+	
+func damage(amt):
+	if !dead:
+		health-=amt
+		knockback(amt)
+		
+		if health <= 0:
+			health = 0
+			death()
+		
+		$healthBar.value = health
+		
+	
+func knockback(intensity):
+	var kbVector = (300*intensity) * (-towardsPlayer).normalized()
+	apply_central_impulse(kbVector)
+	inKB = true
+	$knockback.start()
+
+
+func _on_knockback_timeout() -> void:
+	inKB = false
+
+
+func _on_death_timer_timeout() -> void:
+	queue_free()
