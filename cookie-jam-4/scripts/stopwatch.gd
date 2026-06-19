aee399facaf0f8a55fee47ad31da1650b63558f0
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-18 20:51:54 -0700

    cemetary background

--- cookie-jam-4/scripts/stopwatch.gd
+++ cookie-jam-4/scripts/stopwatch.gd
@@ -1,17 +1,25 @@
 extends Control
 
+var inSlow = false
+var slowLength = 0
 
+@onready var hand = $watchCenter
 func startTime(length):
-	Engine.time_scale = 0.3
-	t = length
-	
+	if !inSlow:
+		inSlow = true
+		slowLength = length
+		Engine.time_scale = 0.3
+		t = length
+		
 
 var t = 0
 func _physics_process(delta: float) -> void:
 	if t!=0:
 		t -= 1/50.0
+		hand.rotation_degrees += 360/(50.0*slowLength)
 	if t < 0:
-		print("end time")
 		#end timeslow
+		hand.rotation_degrees = 0
+		inSlow=false
 		Engine.time_scale = 1
 		t=0
