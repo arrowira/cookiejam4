5f1291210098f8aecb76ff2c4ef5aa0fb2a26741
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-20 15:19:49 -0700

    speed ring error and stopwatch cd

--- cookie-jam-4/scripts/speed_circle.gd
+++ cookie-jam-4/scripts/speed_circle.gd
@@ -20,6 +20,18 @@ func _physics_process(delta: float) -> void:
 			$speed.play()
 			$expander.scale *= 1.2
 			get_parent().changeSpinSpeed(winBoost)
+		else:
+			$CD.start()
+			$err.play()
+			modulate.r = 1
+			modulate.g = 0.3
+			modulate.b = 0.3
 	
 	if $expander.scale.length() >= 1.5:
 		$expander.scale *= 0.99
+
+
+func _on_cd_timeout() -> void:
+	modulate.r = 1
+	modulate.g = 1
+	modulate.b = 1
--- cookie-jam-4/scripts/stopwatch.gd
+++ cookie-jam-4/scripts/stopwatch.gd
@@ -2,10 +2,11 @@ extends Control
 
 var inSlow = false
 var slowLength = 0
+var inCD = false
 
 @onready var hand = $watchCenter
 func startTime(length):
-	if !inSlow:
+	if !inSlow and !inCD:
 		$"../../music".slow()
 		$"../../audio/timeSlow".play()
 		$vigFade.play("in")
@@ -22,10 +23,18 @@ func _physics_process(delta: float) -> void:
 		hand.rotation_degrees += 360/(50.0*slowLength)
 	if t < 0:
 		#end timeslow
+		$CD.start()
 		$"../../music".normal()
 		$"../../audio/timeSlow".stop()
 		$vigFade.play("out")
+		inCD = true
+		modulate.a = 0.3
 		hand.rotation_degrees = 0
 		inSlow=false
 		Engine.time_scale = 1
 		t=0
+
+
+func _on_cd_timeout() -> void:
+	inCD=false
+	modulate.a = 1
