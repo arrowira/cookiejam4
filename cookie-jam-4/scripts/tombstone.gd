7d9eca42834713f0b515b1e0761ca273cd1cb671
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-19 18:59:56 -0700

    tombstone spawning

--- cookie-jam-4/scripts/tombstone.gd
+++ cookie-jam-4/scripts/tombstone.gd
@@ -4,13 +4,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Sprite2D.frame = randi_range(1,5)*5
+	z_index = global_position.y/100.0
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$shadow.frame = $Sprite2D.frame
-	z_index = global_position.y/100.0
 
 func _break():
 	$body.queue_free()
-	$Sprite2D.frame = randi_range(1,5)*5+4
+	$Sprite2D.frame = randi_range(1,5)*5+3
