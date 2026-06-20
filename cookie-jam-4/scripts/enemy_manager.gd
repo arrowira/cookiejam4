b33a36d363e6d2dca4a310f90cb3a5ee3be0b335
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-20 02:59:37 -0700

    added grunt spawn animation

--- cookie-jam-4/scripts/enemy_manager.gd
+++ cookie-jam-4/scripts/enemy_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var pGrunt = preload("res://scenes/grunt.tscn")
 var pNecro = preload("res://scenes/necromancer.tscn")
+var pWizarda = preload("res://scenes/wizardA.tscn")
 var playerPos 
 var distance = 1500
 
@@ -16,11 +17,17 @@ func _process(delta: float) -> void:
 	playerPos = get_parent().get_node("player").anchorPos
 
 func _on_spawn_cd_timeout() -> void:
-	var fate = randi_range(1,5)
+	var fate = randi_range(1,10)
 	var angle = randf_range(0,2*PI)
 	
-	if fate == 1:
+	if fate == 1 or fate == 2:
 		population+=1
 		var necro = pNecro.instantiate()
 		necro.global_position = playerPos+distance*Vector2(cos(angle),sin(angle))
 		add_child(necro)
+	if fate == 3:
+		population+=1
+		var wiz = pWizarda.instantiate()
+		wiz.global_position = playerPos+distance*Vector2(cos(angle),sin(angle))
+		add_child(wiz)
+		
