b1364e7e78322c3e0c97fe705a3958e09321e8d1
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-19 17:45:45 -0700

    projectile rewrite using rational functions

--- cookie-jam-4/scripts/projectile.gd
+++ cookie-jam-4/scripts/projectile.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
-var speed = 30
+var speed = 0
+var targetSpeed = 10.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+var t = 0
 func _physics_process(delta: float) -> void:
+	t+=0.1
 	position += speed*Vector2(-cos(rotation), -sin(rotation))
 	
 	#friction
-	speed*=0.99
+	speed = (targetSpeed*t+1)/(t+3)*10
 	
 
 
