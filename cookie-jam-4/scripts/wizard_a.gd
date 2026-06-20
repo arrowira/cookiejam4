3eca963527cea28a6250daf4f7bb9c348720ae21
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-19 17:31:37 -0700

    wizard A casting animation logic

--- cookie-jam-4/scripts/wizard_a.gd
+++ cookie-jam-4/scripts/wizard_a.gd
@@ -8,7 +8,12 @@ var frozen = false
 var dead = false
 
 var pGhost = preload("res://scenes/grunt_ghost.tscn")
-var speed = 1.5
+var speed = 0.5
+
+var t = 0
+var isWalking = true
+var isCasting = false
+var orbIn = false
 
 func _ready() -> void:
 	$healthBar.value = 100
@@ -18,22 +23,33 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !frozen:
-		$AnimationPlayer.play("walk")
-		if player.x<global_position.x:
-			$Icon.flip_h=false
-			$Icon/shadow.position = Vector2(0.8,7.8)
+		if isWalking:
+			$AnimationPlayer.play("walk")
+			if player.x<global_position.x:
+				$Icon.flip_h=false
+				$Icon/shadow.position = Vector2(0.8,16.3)
+			else:
+				$Icon.flip_h=true
+				$Icon/shadow.position = Vector2(-0.8,16.3)
 		else:
-			$Icon.flip_h=true
-			$Icon/shadow.position = Vector2(-0.8,7.8)
+			if !isCasting:
+				$AnimationPlayer.play("RESET")
 		
 		player = get_parent().get_parent().get_node("player").anchorPos
 	
 
 func _physics_process(delta: float) -> void:
+	t+=0.01
 	rotation = 0
-	if !inKB and !frozen:
+	if !inKB and !frozen and isWalking:
 		towardsPlayer = (player-global_position).normalized()
-		position+=towardsPlayer*speed*Engine.time_scale
+		position+=towardsPlayer*speed*Engine.time_scale + (1.0)*Vector2(cos(t),sin(t))
+
+
+func cast():
+	isCasting=true
+	$castTime.start()
+	$AnimationPlayer.play("cast")
 
 func death():
 	dead = true
@@ -72,3 +88,17 @@ func _on_knockback_timeout() -> void:
 
 func _on_death_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_behavior_timeout() -> void:
+	if randf() < 0.3:
+		isWalking=false
+	else:
+		if !isCasting:
+			isWalking=true
+	if !isWalking and randf() < 0.3:
+		cast()
+
+
+func _on_cast_time_timeout() -> void:
+	isCasting = false
