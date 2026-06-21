dfc16ea5235c673113228aa877caaed6761fcede
arrowira <41136800+arrowira@users.noreply.github.com>
2026-06-20 19:15:24 -0700

    start menu first panel

---
+++ cookie-jam-4/scripts/start_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func setting():
+	pass
+func howTo():
+	pass
+func play():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_start_b_button_down() -> void:
+	pass # Replace with function body.
